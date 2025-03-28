705b72e8a3b676ef7ce42982e36fc2ce7db2969c
dhph17 <102210114@sv1.dut.udn.vn>
2025-03-29 01:12:00 +0700

    initial setup

---
+++ babel.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  presets: ['@babel/preset-typescript'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@': './src',
+        },
+      },
+    ],
+  ],
+}; 
---
+++ src/main.ts
@@ -0,0 +1,49 @@
+import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+import { AppModule } from '@/app.module';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { useContainer } from 'class-validator';
+import { AllConfigType } from '@/shared/infrastructure/config/config.type';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
+  const configService = app.get(ConfigService<AllConfigType>);
+
+  app.enableCors({
+    origin: configService.getOrThrow('app.frontendDomain', { infer: true }),
+    credentials: true,
+  });
+
+  app
+    .enableShutdownHooks()
+    .useGlobalPipes(new ValidationPipe())
+    .enableVersioning({
+      type: VersioningType.URI,
+    })
+    .setGlobalPrefix(configService.getOrThrow('app.apiPrefix', { infer: true }), {
+      exclude: ['/'],
+    });
+
+  const config = new DocumentBuilder()
+    .setTitle('API Documentation')
+    .setDescription('API Documentation')
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        description: 'Enter JWT token',
+      },
+      'access-token',
+    )
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(configService.getOrThrow('app.port', { infer: true }));
+  console.log(`Server is running on port ${configService.getOrThrow('app.port', { infer: true })}`);
+}
+bootstrap();

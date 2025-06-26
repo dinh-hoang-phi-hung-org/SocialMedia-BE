99447c58f85ff2c781291530903f0d79eeb05238
dhph17 <102210114@sv1.dut.udn.vn>
2025-06-26 23:59:33 +0700

    hotfix load conversation

--- src/modules/message/application/use-cases/get-conversations.use-case.ts
+++ src/modules/message/application/use-cases/get-conversations.use-case.ts
@@ -3,22 +3,12 @@ import { ConversationRepository } from '../../infrastructure/repositories/conver
 import { SearchOptions } from '@/shared/types/search-options';
 import { PaginatedResult } from '@/shared/types/paginated-result.interface';
 import { ConversationOrmEntity } from '../../infrastructure/orm/conversation.entity.orm';
-import { UserConversationRepository } from '../../infrastructure/repositories/user-conversation.repository';
+
 @Injectable()
 export class GetConversationsUseCase {
-  constructor(
-    private readonly conversationRepository: ConversationRepository,
-    private readonly userConversationRepository: UserConversationRepository,
-  ) {}
+  constructor(private readonly conversationRepository: ConversationRepository) {}
 
   async execute(userId: string, query: SearchOptions): Promise<PaginatedResult<ConversationOrmEntity>> {
-    const userConversations = await this.userConversationRepository.getUserConversations(userId, query);
-    console.log(userConversations);
-    const conversationUuids = userConversations.data.map((userConversation) => userConversation.conversationUuid);
-    const conversations = await this.conversationRepository.findByUuids(conversationUuids);
-    return {
-      data: conversations,
-      meta: userConversations.meta,
-    };
+    return this.conversationRepository.getUserConversationsWithPagination(userId, query);
   }
 }

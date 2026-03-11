33f6776ebf7f439e9722718d45bfa0b378c40dba
dh-phihung <dh-phihung@pasonatech.vn>
2026-03-11 10:40:35 +0700

    fix: update migration commands in package.json to use direct typeorm calls

--- src/typeorm.config.ts
+++ src/typeorm.config.ts
@@ -2,15 +2,7 @@ import { DataSource } from 'typeorm';
 import type { DataSourceOptions } from 'typeorm';
 import { join } from 'path';
 import * as dotenv from 'dotenv';
-import { register } from 'tsconfig-paths';
 import type { SeederOptions } from 'typeorm-extension';
-
-register({
-  baseUrl: './src',
-  paths: {
-    '@/*': ['*'],
-  },
-});
 
 dotenv.config();
 
@@ -21,13 +13,13 @@ const option: DataSourceOptions & SeederOptions = {
   username: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
-  entities: [join('dist', '**', '*.entity.orm.js')],
-  migrations: [join('dist', 'shared', 'infrastructure', 'database', 'migrations', '*.{js,ts}')],
+  entities: [join(__dirname, '**', '*.entity.orm.{js,ts}')],
+  migrations: [join(__dirname, 'shared/infrastructure/database/migrations/*.js')],
   migrationsTableName: 'migrations',
   synchronize: false,
   logging: true,
   seedTracking: true,
-  seeds: [join('dist', 'shared', 'infrastructure', 'database', 'seeds', '*.js')],
+  seeds: [join(__dirname, 'shared/infrastructure/database/seeds/*.{js,ts}')],
 };
 
 export default new DataSource(option);
